af7100364234dfd0bf25519ecfd68e233a550f85
rizeuscu <rizescubogdan2001@gmail.com>
2023-05-03 17:00:21 +0300

    added unittests

--- tree.py
+++ tree.py
@@ -1,4 +1,5 @@
 from node import Node
+import unittest
 
 
 class Tree:
@@ -54,6 +55,14 @@ class Tree:
             return None
 
     def _find(self, data, node):
+        """Method for find data in the tree
+
+        Args:
+            data (int): data to find
+
+        Returns:
+            Node: node with data
+        """
         if data == node.data:
             return node
         elif (data < node.data and node.left is not None):
@@ -62,16 +71,37 @@ class Tree:
             return self._find(data, node.right)
 
     def deleteTree(self):
-        # TODO 1
+        """Method for find data in the tree
+
+        Args:
+            data (int): data to find
+
+        Returns:
+            Node: node with data
+        """
         self.root = None
 
     def printTree(self):
-        # TODO 1
+        """Method for find data in the tree
+
+        Args:
+            data (int): data to find
+
+        Returns:
+            Node: node with data
+        """
         if self.root is not None:
             self._printInorderTree(self.root)
 
     def _printInorderTree(self, node):
-        # TODO 1
+        """Method for find data in the tree
+
+        Args:
+            data (int): data to find
+
+        Returns:
+            Node: node with data
+        """
         if node is not None:
             self._printInorderTree(node.left)
             print(str(node.data) + ' ')
@@ -79,10 +109,63 @@ class Tree:
 
     def _printPreorderTree(self, node):
         # TODO 2
-        pass
+        """Method for find data in the tree
+
+        Args:
+            data (int): data to find
+
+        Returns:
+            Node: node with data
+        """
+        if node is not None:
+            print(str(node.data) + ' ')
+            self._printInorderTree(node.left)
+            self._printInorderTree(node.right)
 
     def _printPostorderTree(self, node):
         # TODO 2
-        pass
+        """Met/rizeuscu/tree-traversal/actionshod for find data in the tree
+
+        Args:
+            data (int): data to find
+
+        Returns:
+            Node: node with data
+        """
+        if node is not None:
+            self._printInorderTree(node.left)
+            self._printInorderTree(node.right)
+            print(str(node.data) + ' ')
+
+class TestTree(unittest.TestCase):
+    """ Test class for Tree class """
+    
+    def test__find(self):
+        """ Method for test find method """
+        tree = Tree()
+        tree.add(3)
+        tree.add(4)
+        tree.add(0)
+        tree.add(8)
+        tree.add(2)
+        assert tree._find(3, tree.root).data == 3
+        assert tree._find(4, tree.root).data == 4
+        assert tree._find(0, tree.root).data == 0
+        assert tree._find(8, tree.root).data == 8
+        assert tree._find(2, tree.root).data == 2
+    
+    def test__find_2(self):
+        """ Method for test find method """
+        tree = Tree()
+        tree.add(2)
+        tree.add(1)
+        tree.add(4)
+        tree.add(5)
+        tree.add(0)
+        assert tree._find(2, tree.root).data == 2
+        assert tree._find(1, tree.root).data == 1
+        assert tree._find(4, tree.root).data == 4
+        assert tree._find(5, tree.root).data == 5
+        assert tree._find(0, tree.root).data == 0
 
 
