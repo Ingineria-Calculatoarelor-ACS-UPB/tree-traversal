b82cfc7d405d567a25efa9a196701632af13cb5c
alex-constantin <74243061+alex-constantin@users.noreply.github.com>
2023-05-08 12:49:13 +0300

    Create test_tree.py

---
+++ test_tree.py
@@ -0,0 +1,23 @@
+import unittest
+from node import Node
+from tree import Tree
+
+class TestTree(unittest.TestCase):
+    def test_find_data_in_tree(self):
+        tree = Tree()
+        tree.add(5)
+        tree.add(3)
+        tree.add(7)
+        node = tree.find(3)
+        self.assertEqual(node.data, 3)
+
+    def test_find_data_not_in_tree(self):
+        tree = Tree()
+        tree.add(5)
+        tree.add(3)
+        tree.add(7)
+        node = tree.find(10)
+        self.assertIsNone(node)
+
+if _name_ == '_main_':
+    unittest.main()
